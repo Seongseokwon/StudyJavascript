98f95930054eb17e1132abefaef2c678fb27a0a6
Seongseokwon <pam9411@naver.com>
2020-11-25 13:16:22 +0900

    use reduce problem solve

--- compressString.js
+++ compressString.js
@@ -1,52 +1,32 @@
 function compressString(str) {	
-	let count = 1;
-	let incr = 0;
-	let resstr = '';
-	// 지금문자 전문자 같은지 다른지
-	for(let i = 0; i < str.length; i+= incr){
-		console.log("i value : " + i);
-		console.log("incr : " + incr);
-		for(let j = i+1; j < str.length; j++){
-			if(str[i] === str[j]){
-				if(j === str.length-1){
-					count++;
-					if(count >= 3){
-						resstr +=  `${count}${str[j]}`
-						i = str.length;
-						break;
-					}else{
-						for(let k = 0; k < count; k++){
-							resstr += `${str[i]}`
-						}
-						i = str.length;
-						break;
-					}
-				}
-				count++;
-				console.log('count  : ' +count);
+	str = str.split('');
+	str.push(" ");
+	let res = [];
+	str.reduce(function(acc, cur){
+		if(acc[cur]){
+			acc[cur]++;
+		}else{
+			if(acc){
+				res.push(acc);
+				acc = {};
+			}
+			acc[cur] =1;
+		}
+		return acc;
+	},{})
+	let strres = '';
+	for(let i = 0 ; i < res.length; i++){
+		for(let key in res[i]){
+			if(res[i][key] >= 3){
+				strres += `${res[i][key]}${key}`;
 			}else{
-				if(j === str.length -1){
-					resstr += `${str[i]}${str[j]}`;
-					break;
+				for(let j = 0; j < res[i][key]; j++){
+					strres += key;
 				}
-				if(count >= 3){
-					resstr += `${count}${str[i]}`
-					console.log('count >= 3 : ' + resstr);
-				}else{
-					for(let k = 0; k < count; k++){
-						resstr += `${str[i]}`
-					}
-					console.log('else count : ' + resstr);
-				}
-				incr = count;
-				count = 1;
-				console.log('reset : '+ count);
-				break;
 			}
 		}
 	}
-	console.log(resstr);
+	console.log(strres);
 }
-
-let output = compressString('EQTWVOQQQVDVRC');
+let output = compressString('wwwggoppopppp');
 // console.log(output); // --> 3wggoppo4p
